0148d65691eb20b86ea21f232da9321b01adb2ea
Eric <1072538804@qq.com>
2020-12-24 20:23:30 +0800

    删除了echart

--- src/main.js
+++ src/main.js
@@ -9,9 +9,6 @@ import ElementUi from 'element-ui'
 import '../node_modules/element-ui/lib/theme-chalk/index.css'
 Vue.use(ElementUi)
 
-//echart
-import echarts from 'echarts'
-Vue.prototype.$echarts = echarts
 
 Vue.config.productionTip = false
 
